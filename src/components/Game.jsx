bc08820ac8ef835ddae2099fcf39a98cf9b4145f
adriano <adriano2015df@gmail.com>
2024-09-04 10:28:27 -0300

    first commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,16 @@
+
+function Game({ title, cover, onRemove }){
+    return (
+        <div>
+          <img src={cover} alt="Capa do jogo" />
+          <div>
+            <h2>{title}</h2>
+            <button onClick={onRemove}>
+              Remover
+            </button>
+          </div>
+        </div>
+      )
+}
+
+export default Game
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,10 @@
+function TextInput ({ id, label, value, onChange }) {
+    return (
+        <div>
+          <label htmlFor={id}>{label}</label>
+          <input type="text" id={id} value={value} onChange={onChange} />
+        </div>
+      )
+}
+
+export default TextInput
